6ff503c4efa6ae8420165edfb76b1cd0afb94e03
shreynamdeo <shreynamdeo@gmail.com>
2020-01-08 20:58:33 +0530

    File upload functionality to the code.

--- src/main/java/com/jsonUtility/jsonCreator/controllers/UiControllers.java
+++ src/main/java/com/jsonUtility/jsonCreator/controllers/UiControllers.java
@@ -1,15 +1,25 @@
 package com.jsonUtility.jsonCreator.controllers;
 
 import com.jsonUtility.jsonCreator.Dto.UserDto;
+import com.jsonUtility.jsonCreator.model.HRefModel;
+import com.jsonUtility.jsonCreator.services.FileSystemStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.ws.rs.QueryParam;
-import java.util.Optional;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UiControllers {
@@ -17,6 +27,8 @@ public class UiControllers {
     public static String adminEmail = "admin@jsonGenerator.com";
     public static String adminPassword = "passw0rd";
 
+    @Autowired
+    private FileSystemStorageService storageService;
 
     @RequestMapping(value = "/login")
     public String login(Model model , @QueryParam("auth") @DefaultValue("0") Integer auth){
@@ -25,11 +37,58 @@ public class UiControllers {
         return "login";
     }
 
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String uploadFiles(Model model){
+        model.addAttribute("title",title);
+        return "upload";
+    }
+
     @PostMapping("/loginUser")
     public String loginUser(@ModelAttribute UserDto userDto){
         System.out.println(userDto.getEmail());
         if (adminEmail.equals(userDto.getEmail()) && adminPassword.equals(userDto.getPassword()))
-            return "redirect:/adminProductList";
+            return "redirect:/upload";
         return "redirect:/login?auth=1";
+    }
+
+    @RequestMapping(value = "/files/list", method = RequestMethod.GET)
+    public String listFiles(Model model) {
+        List<Path> lodf = new ArrayList<>();
+        List<HRefModel> uris = new ArrayList<>();
+
+        try {
+            lodf = storageService.listSourceFiles(storageService.getUploadLocation());
+            for(Path pt : lodf) {
+                HRefModel href = new HRefModel();
+                href.setHref(MvcUriComponentsBuilder
+                        .fromMethodName(UiControllers.class, "serveFile", pt.getFileName().toString())
+                        .build()
+                        .toString());
+
+                href.setHrefText(pt.getFileName().toString());
+                uris.add(href);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        model.addAttribute("listOfEntries", uris);
+        return "file_list :: urlFileList";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+    @RequestMapping(value = "/files/upload", method = RequestMethod.POST)
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+        return "redirect:/upload";
     }
 }
---
+++ src/main/java/com/jsonUtility/jsonCreator/util/Constants.java
@@ -0,0 +1,7 @@
+package com.jsonUtility.jsonCreator.util;
+
+public class Constants {
+
+	//public static final String UPLOAD_LOCATION = "C:\\java-exec\\upload-dir\\";
+	public static final String UPLOAD_LOCATION = "src/main/resources/static/ExcelFiles";
+}
